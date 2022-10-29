a7ba3e5a07df666ad60799fd10075d860492a02a
Tonilliterafon <115455575+Tonilliterafon@users.noreply.github.com>
2022-10-29 10:36:18 +0200

    Update randomizer.py

--- randomizer.py
+++ randomizer.py
@@ -3,17 +3,20 @@ import random
 x = random.randint(1, 100)
 y = int(input("Adivina el numero escrivint-hi un: "))
 
+torn = int(1)
+
 if y==x :
     print("Enorabona! Ho has aconseguit!")
 
 
-while (y!=x) :
+while (y!=x) :        
     if x > y:
         print("Es petit, torna a provar")
     else:
         print("Es gran, torna a provar")
     y = int(input("Adivina el numero escrivint-hi un una altra vegada: "))
+    torn += 1
 
-print("Enorabona!")
+print("Enorabona! Aconseguit en " , torn , " vegades.")
 
 
