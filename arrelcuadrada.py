34b6c8f11903a375b7b7c2190807971ca9302afc
Tonilliterafon <115455575+Tonilliterafon@users.noreply.github.com>
2023-01-09 11:44:39 +0100

    Update arrelcuadrada.py

--- arrelcuadrada.py
+++ arrelcuadrada.py
@@ -1,12 +1,31 @@
 import math
 
-acabar = False
 
+txt =("el nombre {} quadrat és {} i arrel {:6f}")
+#llista buida
+llista=[]
+#aixó és un token
+acabar = False
 
 print ("Escriu la sequència de naturals. Escriu 0 per acabar la llista:") 
 while ( not acabar  ):
-    nombre = int ( input("Nombre? ") )
+
+    nombre = int(input("Nombre: "))
+    if nombre == 0:
+        acabar = True
+    else:
+        llista.append(nombre)
+print(llista)
+
+for nombre in llista:
+    print(txt.format (nombre,nombre*nombre,math.sqrt(nombre)))
+"""
+while ( acabar = True  ):
+    
     if ( nombre == 0 ):
         acabar = True
     else:
-        print(" El nombre al cuadrat és " + str( nombre*nombre ) + ". La arrel és " + str( math.sqrt( nombre ) ) )
+        #print(" El nombre al cuadrat és " + str( nombre*nombre ) + ". La arrel és " + str(( math.sqrt( nombre )) ) )
+        
+        print(txt.format (nombre,nombre*nombre,math.sqrt(nombre)))
+"""
